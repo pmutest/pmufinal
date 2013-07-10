efa76e3d139d15c341824fcbfded6503d4688c80
vijay <profilemeup@outlook.com>
2013-07-10 17:58:27 +0530

    Global Handler file added to handle image upload

---
+++ PMU/GlobalHandler.ashx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace PMU
+{
+    /// <summary>
+    /// Summary description for GlobalHandler1
+    /// </summary>
+    public class GlobalHandler: IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            //write your handler implementation here.
+            HttpRequest request = context.Request;
+
+            if (IsIE9(request))
+            {
+                byte[] buffer = new byte[request.ContentLength];
+                using (BinaryReader br = new BinaryReader(request.Files["qqfile"].InputStream))
+                    br.Read(buffer, 0, buffer.Length);
+
+                string filename = Path.GetFileName(request.Files["qqfile"].FileName);
+                File.WriteAllBytes(request.PhysicalApplicationPath + filename, buffer);
+                context.Response.Write("{success:true}");
+                context.Response.End();
+            }
+            else
+            {
+                byte[] buffer = new byte[request.ContentLength];
+                using (BinaryReader br = new BinaryReader(request.InputStream))
+                    br.Read(buffer, 0, buffer.Length);
+
+                File.WriteAllBytes(request.PhysicalApplicationPath + request["qqfile"], buffer);
+                context.Response.Write("{success:true}");
+                context.Response.End();
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        private bool IsIE9(HttpRequest request)
+        {
+            return request["qqfile"] == null;
+        }
+    }
+}
